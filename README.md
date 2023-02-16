042d88b1c331be7d31f7161205c9211db6fd90ac
vahe523 <vahetorosyan98@gmail.com>
2023-02-16 23:48:27 +0400

    some_message

---
+++ README.md
@@ -0,0 +1,5 @@
+# Remaining
+# Remaining
+# Remaining
+# Remaining
+# Remaining
---
+++ src/main/java/com/remaining/RemainingApplication.java
@@ -0,0 +1,13 @@
+package com.remaining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemainingApplication.class, args);
+    }
+
+}
